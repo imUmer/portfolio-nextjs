27d92b23504d4f4b42f34c4d823f6c0ef4eabc4c
imUmer <imumerniaz@gmail.com>
2024-10-02 22:52:00 +0300

    changes in index.js

--- data/index.ts
+++ data/index.ts
@@ -205,13 +205,16 @@ export const navItems = [
     {
       id: 1,
       img: "/git.svg",
+      link: "https://github.com/adrianhajdin/zoom-clone"
     },
     {
       id: 2,
       img: "/twit.svg",
+      link: "https://github.com/adrianhajdin/zoom-clone"
     },
     {
       id: 3,
       img: "/link.svg",
+      link: "https://github.com/adrianhajdin/zoom-clone"
     },
   ];
